1464e0bbdae539eeef215ed301b45fbf0083bee3
woodylucas <wlucas0311@gmail.com>
2021-11-26 18:15:02 -0500

    updates

---
+++ components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+import { Link } from "../routes";
+
+const Header = () => {
+  return (
+    <div>
+      <Menu style={{ marginTop: "10px" }}>
+        <Link route="/">
+          <a href="" className="item">
+            CrowdCoin
+          </a>
+        </Link>
+        <Menu.Menu position="right">
+          <Link route="/">
+            <a className="item">Campaigns</a>
+          </Link>
+          <Link route="/campaigns/new">
+            <a className="item">+</a>
+          </Link>
+        </Menu.Menu>
+      </Menu>
+    </div>
+  );
+};
+
+export default Header;
