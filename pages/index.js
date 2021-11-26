5c0560138733868133027dbcb284405cddb8877f
woodylucas <wlucas0311@gmail.com>
2021-11-26 18:15:02 -0500

    updates

---
+++ pages/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Card, Button } from "semantic-ui-react";
+import factory from "../ethereum/factory";
+import Layout from "../components/Layout";
+import { Link } from "../routes";
+
+const CampaignIndex = ({ campaigns }) => {
+  const renderCampaigns = () => {
+    const items = campaigns.map((address) => {
+      return {
+        header: address,
+        description: (
+          <Link route={`/campaigns/${address}`}>
+            <a>View Campaign</a>
+          </Link>
+        ),
+        fluid: true,
+      };
+    });
+    return <Card.Group items={items} />;
+  };
+  return (
+    <Layout>
+      <div>
+        <h3>Open Campaigns</h3>
+        <Link route="/campaigns/new">
+          <a>
+            <Button
+              floated="right"
+              content="Create Campaign"
+              icon="add circle"
+              primary
+            />
+          </a>
+        </Link>
+        {renderCampaigns()}
+      </div>
+    </Layout>
+  );
+};
+
+CampaignIndex.getInitialProps = async () => {
+  const campaigns = await factory.methods.getDeployedCampaigns().call();
+  return { campaigns };
+};
+
+export default CampaignIndex;
