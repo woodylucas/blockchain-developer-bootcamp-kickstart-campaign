d409ab02559713af5da6c4f8a99ab5dc072edea4
woodylucas <wlucas0311@gmail.com>
2021-11-26 21:37:35 -0500

    feat: added semantic css library, and added button css style component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,29 @@
 import React, { useEffect } from "react";
+import { Card, Button } from "semantic-ui-react";
 import factory from "../ethereum/factory";
 
 const CampaignIndex = ({ campaigns }) => {
-  return <div>{campaigns[0]}</div>;
+  const renderCampaigns = () => {
+    const items = campaigns.map((address) => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true,
+      };
+    });
+    return <Card.Group items={items} />;
+  };
+  return (
+    <div>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+      />
+      <h3>Open Campaigns</h3>
+      {renderCampaigns()}
+      <Button content="Create Campaign" icon="add circle" primary />
+    </div>
+  );
 };
 
 CampaignIndex.getInitialProps = async () => {
