dcbcb611e5e766b7864184c5ab6ff89a4634d8b1
woodylucas <wlucas0311@gmail.com>
2021-11-28 14:43:35 -0500

    feat: added routing sysyem

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -3,11 +3,13 @@ import { Form, Button, Input, Message } from "semantic-ui-react";
 import Layout from "../../components/Layout";
 import factory from "../../ethereum/factory";
 import web3 from "../../ethereum/web3";
+import { Router } from "../../routes";
 
 const CampaignNew = () => {
   const [minimumContribution, setMinimumContribution] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+
   const handleSubmit = async (evt) => {
     evt.preventDefault();
     setIsLoading(true);
@@ -17,11 +19,14 @@ const CampaignNew = () => {
       await factory.methods.createCampaign(minimumContribution).send({
         from: accounts[0],
       });
+
+      Router.pushRoute("/");
     } catch (error) {
       setErrorMessage(error.message);
     }
     setIsLoading(false);
   };
+
   return (
     <Layout>
       <h3>Create Campaign</h3>
