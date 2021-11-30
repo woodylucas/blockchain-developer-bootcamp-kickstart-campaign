d877efe70ae674b7979d0d38133fd3d32191df64
woodylucas <wlucas0311@gmail.com>
2021-11-30 16:29:44 -0500

    feat: added table

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Table } from "semantic-ui-react";
 import { Link } from "../../../routes";
 import Layout from "../../../components/Layout";
 import Campaign from "../../../ethereum/campaign";
 
 const RequestIndex = ({ address }) => {
+  const { Header, Row, HeaderCell, Body } = Table;
   return (
     <Layout>
       <h3>Requests</h3>
@@ -13,6 +14,19 @@ const RequestIndex = ({ address }) => {
           <Button primary>Add Request</Button>
         </a>
       </Link>
+      <Table>
+        <Header>
+          <Row>
+            <HeaderCell>ID</HeaderCell>
+            <HeaderCell>Description</HeaderCell>
+            <HeaderCell>Amount</HeaderCell>
+            <HeaderCell>Recipient</HeaderCell>
+            <HeaderCell>Approval Count</HeaderCell>
+            <HeaderCell>Approve</HeaderCell>
+            <HeaderCell>Finalize</HeaderCell>
+          </Row>
+        </Header>
+      </Table>
     </Layout>
   );
 };
